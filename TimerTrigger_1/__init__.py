21504459a348cbdc0eaa533a2e19072c731bb00b
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-05-15 22:22:45 +0200

    use newspaper in azure function

--- TimerTrigger_1/__init__.py
+++ TimerTrigger_1/__init__.py
@@ -2,13 +2,18 @@ import datetime
 import logging
 
 import azure.functions as func
-
+from newspaper import Article
 
 def main(mytimer: func.TimerRequest) -> None:
+
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
+    url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+    article = Article(url)
+    article.download()
+    print(article.authors)
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
