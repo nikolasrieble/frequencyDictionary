58c24dae5be1c3b4c25e557e6d27b00733b803e4
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-05-17 20:31:15 +0200

    MVP

--- TimerTrigger_1/__init__.py
+++ TimerTrigger_1/__init__.py
@@ -1,19 +1,31 @@
 import datetime
 import logging
-
+import pymongo
 import azure.functions as func
-from newspaper import Article
+import newspaper
+import os
+
+url = 'https://www.sozcu.com.tr/'
+language = 'tr'
 
 def main(mytimer: func.TimerRequest) -> None:
-
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
-    url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
-    article = Article(url)
-    article.download()
-    print(article.authors)
+    conn = os.environ.get('MONGO_DB')
+    myclient = pymongo.MongoClient(conn)
+    mydb = myclient['newspaper']
+    collection = mydb[language]
+        
+    paper = newspaper.build(url, language='tr', memoize_articles=False)
+    for article in paper.articles:
+        article.download()
+        article.parse()
+        collection.insert_one({
+        'text' : article.text,
+        'fetched_at' : datetime.datetime.now(),
+        'headline' : article.title,
+        'url': article.url})
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
