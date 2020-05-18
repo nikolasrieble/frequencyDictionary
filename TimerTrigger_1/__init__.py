04b10bc2152605abad26ac3aa1c9d79993cae79e
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-05-18 17:35:21 +0200

    add faz

--- TimerTrigger_1/__init__.py
+++ TimerTrigger_1/__init__.py
@@ -5,8 +5,12 @@ import azure.functions as func
 import newspaper
 import os
 
-url = 'https://www.sozcu.com.tr/'
-language = 'tr'
+
+input_list = [
+    ('tr', 'https://www.sozcu.com.tr/'),
+    ('de', 'https://www.faz.net/')
+    ]
+
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -17,15 +21,18 @@ def main(mytimer: func.TimerRequest) -> None:
     conn = os.environ.get('MONGO_DB')
     myclient = pymongo.MongoClient(conn)
     mydb = myclient['newspaper']
-    collection = mydb[language]
-        
-    paper = newspaper.build(url, language='tr', memoize_articles=False)
-    for article in paper.articles:
-        article.download()
-        article.parse()
-        collection.insert_one({
-        'text' : article.text,
-        'fetched_at' : datetime.datetime.now(),
-        'headline' : article.title,
-        'url': article.url})
+
+    for language, url in input_list:
+        collection = mydb[language]
+
+        paper = newspaper.build(url, language=language, memoize_articles=False)
+        for article in paper.articles:
+            article.download()
+            article.parse()
+            collection.insert_one({
+            'text' : article.text,
+            'fetched_at' : datetime.datetime.now(),
+            'headline' : article.title,
+            'url': article.url})
+            
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
