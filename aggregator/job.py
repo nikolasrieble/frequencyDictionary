a8f06caa513c8d021ff4ad2e0d2b5bf0d37e4e77
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-05-24 20:27:24 +0200

    HelloMongo works locally

--- aggregator/job.py
+++ aggregator/job.py
@@ -1,2 +1,11 @@
 from datetime import datetime
-print(datetime.now())
+import pymongo
+
+def helloMongo():
+    MONGO_DB = "mongodb+srv://newspaperScraper:eQUoQcPeMZdA2jze@cluster1-qrhh1.azure.mongodb.net/test?retryWrites=true&w=majority"
+    myclient = pymongo.MongoClient(MONGO_DB)
+    mydb = myclient['statistics']
+    mycol = mydb['summary']
+    mycol.insert_one({"test": datetime.now()})
+
+helloMongo()
--- aggregator/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Write environment variables into file, so they can be made available to cron jobs
-declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
-
-cron
