ce1804819fe826b7db6a1f3f852cbcedb58fbf23
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-04 18:50:50 +0200

    Integrate scraper into web app

---
+++ Scraper/README.md
@@ -0,0 +1,3 @@
+`docker build -t scraper .`
+
+`docker run -p 5000:5000 scraper`
--- Scraper/src/server.py
+++ Scraper/src/server.py
@@ -1,12 +1,19 @@
+import datetime
+
+import newspaper
 from flask import Flask
 from flask_apscheduler import APScheduler
 
-import time
+from Scraper.src.database import Database
+
+
+class Config(object):
+    SCHEDULER_API_ENABLED = True
+
 
 app = Flask(__name__)
 scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
+database = Database()
 
 
 @app.route('/')
@@ -14,18 +21,45 @@ def welcome():
     return 'Welcome to flask_apscheduler demo', 200
 
 
-@app.route('/run-tasks')
-def run_tasks():
-    for i in range(10):
-        app.apscheduler.add_job(func=scheduled_task, trigger='date', args=[i], id='j' + str(i))
-
-    return 'Scheduled several long running tasks.', 200
+@app.route('/trigger_scraper')
+def trigger_scrape():
+    scrape()
+    return 'Scraping triggered', 200
 
 
-def scheduled_task(task_id):
-    for i in range(10):
-        time.sleep(1)
-        print('Task {} running iteration {}'.format(task_id, i))
+@scheduler.task('interval', id='health', minutes=10, misfire_grace_time=900)
+def health():
+    print("I am healthy")
 
 
-app.run(host='0.0.0.0', port=12345)
+@scheduler.task('interval', id='scrape', hours=12, misfire_grace_time=900)
+def scrape():
+    for language, url in [
+        ('de', 'https://www.faz.net/')
+    ]:
+        paper = newspaper.build(url, language=language, memoize_articles=False)
+        for article in paper.articles:
+            article.download()
+            article.parse()
+
+            database.insert(
+                {
+                    'text': article.text,
+                    'fetched_at': datetime.datetime.now(),
+                    'headline': article.title,
+                    'url': article.url
+                }
+            )
+
+    print('Ran scrape function')
+
+
+if __name__ == '__main__':
+    app.config.from_object(Config())
+
+    # it is also possible to enable the API directly
+    # scheduler.api_enabled = True
+    scheduler.init_app(app)
+    scheduler.start()
+
+    app.run(host='0.0.0.0', port=12345)
