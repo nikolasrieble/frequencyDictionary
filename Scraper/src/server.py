0b3aa5fba5bda05661fe3d2d598996943e956cd3
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-05 20:20:59 +0200

    Add scraping target that can be configured by environment variables and updated without redeployment

--- Scraper/src/server.py
+++ Scraper/src/server.py
@@ -11,18 +11,28 @@ from newspaper import ArticleException
 from configuration import Configuration
 from database import Database
 
-with open('configuration.json', 'r') as json_file:
+with open('../configuration.json', 'r') as json_file:
     raw_config = json_file.read()
 Config = dacite.from_dict(data_class=Configuration, data=json.loads(raw_config))
 
 app = Flask(__name__)
+app.target = None
 scheduler = APScheduler()
 database = Database(Config.database)
+
+DEFAULT_TARGET = '[{"language":"de", "url":"https://www.faz.net/"}]'
+DEFAULT_PORT = 5000
 
 
 @app.route('/')
 def welcome():
     return 'Welcome to flask_apscheduler demo', 200
+
+
+@app.route('/test_scraper')
+def trigger_scrape():
+    scrape(limit=1)
+    return 'Scraping test triggered', 200
 
 
 @app.route('/trigger_scraper')
@@ -31,18 +41,27 @@ def trigger_scrape():
     return 'Scraping triggered', 200
 
 
+@app.route('/reload_target')
+def reload_target():
+    load_target()
+    return 'Target set to {}'.format(app.target), 200
+
+
 @scheduler.task('interval', id='health', minutes=10, misfire_grace_time=900)
 def health():
     print("I am healthy")
 
 
 @scheduler.task('interval', id='scrape', hours=12, misfire_grace_time=900)
-def scrape():
-    for language, url in [
-        ('de', 'https://www.faz.net/')
-    ]:
-        paper = newspaper.build(url, language=language, memoize_articles=False)
-        for article in paper.articles:
+def scrape(limit: int = None):
+    for target in app.target:
+        print(target['url'], target['language'])
+        paper = newspaper.build(target['url'], language=target['language'], memoize_articles=False)
+        for idx, article in enumerate(paper.articles):
+
+            if limit:
+                if idx > limit:
+                    break
 
             try:
                 article.download()
@@ -61,13 +80,18 @@ def scrape():
     print('Ran scrape function')
 
 
+def load_target():
+    target_configuration = os.environ.get('TARGET', DEFAULT_TARGET)
+    app.target = json.loads(target_configuration)
+
+
 if __name__ == '__main__':
     app.config.from_object(Config.backend)
-
+    load_target()
     # it is also possible to enable the API directly
     # scheduler.api_enabled = True
     scheduler.init_app(app)
     scheduler.start()
 
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', DEFAULT_PORT))
     app.run(host='0.0.0.0', port=port)
