d0a99b4f578b0cd0403a693f692e8039c0e3f20a
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-05 12:45:43 +0200

    Fix heroku related port binding

--- Scraper/README.md
+++ Scraper/README.md
@@ -1,3 +1,11 @@
+Local test 
+
 `docker build -t scraper .`
 
 `docker run -p 5000:5000 scraper`
+
+Deployment 
+
+`heroku container:push web -a scraping-backend`
+
+`heroku container:release web -a scraping-backend`
--- Scraper/src/server.py
+++ Scraper/src/server.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 
 import dacite
 import newspaper
@@ -68,4 +69,5 @@ if __name__ == '__main__':
     scheduler.init_app(app)
     scheduler.start()
 
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
