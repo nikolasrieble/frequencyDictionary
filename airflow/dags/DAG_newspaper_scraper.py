6dd48a5353d76fc74301583f88038a26d1b15dd1
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-07 19:16:59 +0200

    Removing default file due to import error

--- airflow/dags/DAG_newspaper_scraper.py
+++ airflow/dags/DAG_newspaper_scraper.py
@@ -1,11 +1,22 @@
-import datetime
 import newspaper
-import os
 import pymongo
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
+import datetime
+import os
 
-from airflow.default import default_args
+default_args = {
+    'owner': 'niko_huy',
+    'start_date': datetime.datetime(2020, 2, 18),
+    'provide_context': True,
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+    'execution_timeout': datetime.timedelta(minutes=60),
+    'pool': 'default_pool',
+    'templates_dict': {
+        'mongodb_string': os.environ.get('MONGO_DB'),
+    }
+}
 
 input_list = {
     'tr': 'https://www.sozcu.com.tr/',
